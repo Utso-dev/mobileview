a6ef6ecac7a2a272f87a27f4bcfee560354e08ce
Utso sarkar <utsosarkar7870@gmail.com>
2023-09-13 18:59:56 +0600

    mobileview

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 const call = document.querySelector(".call")
 const callNow = document.querySelector(".icon-call")
 const mail = document.querySelector(".mail")
@@ -6,6 +7,7 @@ const loc = document.querySelector(".locat")
 const locationNow = document.querySelector(".icon-locat")
  const web = document.querySelector(".webs")
 const webNow = document.querySelector(".icon-web")
+const Home = document.querySelector("#Home")
 
 const overlay = document.querySelector(".content-overlay")
 const Textoverlay = document.querySelector(".text-overlay")
@@ -13,91 +15,84 @@ const notification = document.querySelector(".notification")
 const menu = document.querySelector(".menu")
 const wifi = document.querySelector(".wifi")
 
-const move = ()=>{
-  call.addEventListener("click", function(){
-    callNow.style.display = "block";
-    mailNow.style.display = "none";
-    locationNow.style.display = "none";
-    webNow.style.display = "none";
-    notification.style.height ="95px"
-  })
-  mail.addEventListener("click", function(){
+call.addEventListener("click", function(){
+  callNow.style.display = "block";
+  mailNow.style.display = "none";
+  locationNow.style.display = "none";
+  webNow.style.display = "none";
+   
+})
+mail.addEventListener("click", function(){
+ callNow.style.display = "none";
+  mailNow.style.display = "block";
+  locationNow.style.display = "none";
+  webNow.style.display = "none";
+})
+ loc.addEventListener("click", function(){
    callNow.style.display = "none";
-    mailNow.style.display = "block";
-    locationNow.style.display = "none";
-    webNow.style.display = "none";
-    notification.style.height ="99px"
+  mailNow.style.display = "none";
+  locationNow.style.display = "block";
+  webNow.style.display = "none"; 
+  
   })
-   loc.addEventListener("click", function(){
-     callNow.style.display = "none";
-    mailNow.style.display = "none";
-    locationNow.style.display = "block";
-    webNow.style.display = "none";
-    notification.style.height ="79px"
-    })
-  web.addEventListener("click", function(){
-   callNow.style.display = "none";
-    mailNow.style.display = "none";
-    locationNow.style.display = "none";
-    webNow.style.display = "block";
-    notification.style.height ="95px"
-  },true)
-}
+web.addEventListener("click", function(){
+ callNow.style.display = "none";
+  mailNow.style.display = "none";
+  locationNow.style.display = "none";
+  webNow.style.display = "block";
+})
+
+const welcome=document.querySelector('.container')
 
 
-overlay.addEventListener("mouseover", function(){
-
-    call.addEventListener("click", function(){
-        callNow.style.display = "block";
-        mailNow.style.display = "none";
-        locationNow.style.display = "none";
-        webNow.style.display = "none";
-        notification.style.height = "72%" 
-      })
-      mail.addEventListener("click", function(){
-       callNow.style.display = "none";
-        mailNow.style.display = "block";
-        locationNow.style.display = "none";
-        webNow.style.display = "none";
-        notification.style.height = "72%"  
-      })
-       loc.addEventListener("click", function(){
-         callNow.style.display = "none";
-        mailNow.style.display = "none";
-        locationNow.style.display = "block";
-        webNow.style.display = "none"; 
-        notification.style.height = "72%"  
-        })
-      web.addEventListener("click", function(){
-       callNow.style.display = "none";
-        mailNow.style.display = "none";
-        locationNow.style.display = "none";
-        webNow.style.display = "block";
-        notification.style.height = "72%"  
-      })
+let isOpen=false;
+welcome.addEventListener("click", function(e){
+        e.preventDefault()
+       
+      if(isOpen){
+        menu.style.display="block"
+        wifi.style.display="block"
+         overlay.classList.remove("content-to");
+         Textoverlay.classList.remove("to")
+         notification.classList.remove("Notification-to")
+         isOpen=false;
+         return false;
+      }
         callNow.style.display = "none";
         mailNow.style.display = "none";
         locationNow.style.display = "none";
         webNow.style.display = "none";
-        
-    wifi.style.display="none"
-    menu.style.display="none"
-    overlay.classList.add("content-to");
-    Textoverlay.classList.add("to")
-    notification.classList.add("Notification-to") 
+        isOpen=true;
+       wifi.style.display="none"
+      menu.style.display="none"
+      overlay.classList.add("content-to");
+      Textoverlay.classList.add("to")
+      notification.classList.add("Notification-to") 
 })
+// active item===============
+let list = document.querySelectorAll(".home-icon  li");
+function active(){
+  list.forEach((item)=>{
+    item.classList.remove("active")
 
-overlay.addEventListener("mouseout", function(){
-  menu.style.display="block"
-    wifi.style.display="block"
-    overlay
-    Textoverlay
-    notification
-     overlay.classList.remove("content-to");
-     Textoverlay.classList.remove("to")
-     notification.classList.remove("Notification-to")
-     notification.style.height = ""  
-})
+    this.classList.add("active")
+  })
+}
+list.forEach((item)=> item.addEventListener("click",active))
+
+
+
+// overlay.addEventListener("mouseout", function(){
+//   menu.style.display="block"
+//     wifi.style.display="block"
+//     overlay
+//     Textoverlay
+//     notification
+//      overlay.classList.remove("content-to");
+//      Textoverlay.classList.remove("to")
+//      notification.classList.remove("Notification-to")
+//      notification.style.height = ""  
+// })
 
 
   //  const over = document.getElementById("over");
